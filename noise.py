ead7b50c80ff220ccb780a4cc9899b98ea2eb90f
Alex Pagan <alexrpagan@gmail.com>
2013-10-16 12:24:24 -0400

    Better smoothing and cleanup

--- noise.py
+++ noise.py
@@ -1,12 +1,13 @@
 import pyaudio
 import audioop
 import struct
-import numpy
 import wave
 import sys
+import numpy
 from Queue import Queue
 from threading import Thread
 from collections import deque
+
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
@@ -14,69 +15,64 @@ CHANNELS = 2
 RATE = 44100
 INPUT_BLOCK_TIME = 0.05
 INPUT_FRAMES_PER_BLOCK = int(RATE*INPUT_BLOCK_TIME)
-WINDOW_SIZE = .5
-MAX = 500
+WINDOW_SIZE = 10
+MAX = 300
+CLEANUP = -1
+
 
 def play_audio(filename, q):
-
     wf = wave.open(filename, 'rb')
-
     p = pyaudio.PyAudio()
+    stream = p.open(format = p.get_format_from_width(wf.getsampwidth()),
+                    channels = wf.getnchannels(),
+                    rate = wf.getframerate(),
+                    output = True)
 
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True)
-
+    # bring file into memory
     data = wf.readframes(CHUNK)
-
     chunks = []
     while data != '':
         chunks.append(data)
         data = wf.readframes(CHUNK)
+    wf.close()
 
     loudness = 1
-
-    window = deque([1],maxlen=5)
-
     while True:
         for chunk in chunks:
             if not q.empty():
                 loudness = q.get()
-                if loudness == -1:
+                if loudness == CLEANUP:
                     stream.stop_stream()
                     stream.close()
                     p.terminate()
                     q.task_done()
                     return
-                else:
-                    window.append(loudness)
-            adjustment = float(sum(window))/len(window)
-            s = numpy.fromstring(chunk, numpy.int16) * adjustment
-            s = struct.pack('h'*len(s), *s)
-            stream.write(s)
+            s = numpy.fromstring(chunk, numpy.int16) * loudness
+            stream.write(struct.pack('h'*len(s), *s))
+
 
 def find_input_device(p):
     device_index = None
-    for i in range( p.get_device_count() ):
+    for i in range(p.get_device_count()):
         devinfo = p.get_device_info_by_index(i)
         for keyword in ["mic","input"]:
             if keyword in devinfo["name"].lower():
-                device_index = i
                 return device_index
     if device_index == None:
         return None
     return device_index
 
+
 def open_mic_stream(p):
     device_index = find_input_device(p)
-    stream = p.open(  format = FORMAT,
-                            channels = CHANNELS,
-                            rate = RATE,
-                            input = True,
-                            input_device_index = device_index,
-                            frames_per_buffer = INPUT_FRAMES_PER_BLOCK)
+    stream = p.open(format = FORMAT,
+                    channels = CHANNELS,
+                    rate = RATE,
+                    input = True,
+                    input_device_index = device_index,
+                    frames_per_buffer = INPUT_FRAMES_PER_BLOCK)
     return stream
+
 
 def main():
 
@@ -92,19 +88,19 @@ def main():
 
     stream = open_mic_stream(p)
 
-    samples = []
+    # rolling window of samples of room noise
+    samples = deque([1], maxlen=int(WINDOW_SIZE / INPUT_BLOCK_TIME))
 
     try:
         while 1:
-                if samples and len(samples) % int(WINDOW_SIZE / INPUT_BLOCK_TIME) == 0:
-                    mean = numpy.mean(samples)
-                    q.put(float(mean/MAX))
-                    samples = []
-                block = stream.read(INPUT_FRAMES_PER_BLOCK)
-                amplitude = audioop.rms(block, 2)
-                samples.append(amplitude)
+            mean = numpy.mean(samples)
+            print mean
+            q.put(mean/MAX)
+            block = stream.read(INPUT_FRAMES_PER_BLOCK)
+            amplitude = audioop.rms(block, 2)
+            samples.append(amplitude)
     except KeyboardInterrupt:
-        q.put(-1)
+        q.put(CLEANUP)
         stream.stop_stream()
         stream.close()
         p.terminate()
@@ -112,4 +108,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
